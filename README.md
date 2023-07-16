a36e90991c7aba26369c01fb04ee6f8e48feff28
Davi Nascimento <davi41229@outlook.com>
2023-07-16 07:33:45 +0000

    atualizaçoes-iniciais

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ aplicacao em flask usando o Gitpod.io
 
 
 - tela com formulario de satisfação
+- modelo MVC
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from app import app
+from app.controller import controller_default
 
+if __name__ == "__main__":
+    app.run(debug=True)
